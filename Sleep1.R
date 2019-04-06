e79e359c3a98c109694ec8a710502e5c554f2179
sophiehiscock <32620065+sophiehiscock@users.noreply.github.com>
2019-04-06 12:26:02 +0100

    More work on initial models

--- Sleep1.R
+++ Sleep1.R
@@ -1,6 +1,17 @@
 #dat<-read.csv("sleepedit.csv",header=T) #pre-edited data, without aliased coefficients
 dat<-read.csv("sleep.csv", header=T)
 head(dat)
+mydata = round(cor(dat),2)
+cor(dat)
+install.packages("corrplot")
+library(corrplot)
+palette = colorRampPalette(c("green", "white", "red")) (50)
+heatmap(x = mydata, col = palette, symm = TRUE)
+write.csv(mydata,row.names = TRUE,"AllVarCor.csv")
+install.packages("Hmisc")
+library("Hmisc")
+mydata.rcorr = rcorr(as.matrix(mydata))
+mydata.rcorr
 
 #turning the categorical variables into numbers within the code:
 dat$LarkOwl<-relevel(dat$LarkOwl,ref="Neither")
@@ -27,10 +38,35 @@ vif(m.b)
 #VIF of 1 means not correlated, VIF of more than 10 highly correlated
 
 #Model with one variable from each "group"
-m.c<-lm(CognitionZscore~ Gender+ClassYear+ClassesMissed+AverageSleep+DASScore+AlcoholUse+GPA, data=dat) 
-summary(m.c)
-vif(m.c) #no issues of collinearity here, remove confounding variable
-plot(m.c)
+m.group1<-lm(CognitionZscore~ Gender+ClassYear+ClassesMissed+AverageSleep+DASScore+AlcoholUse+GPA, data=dat) 
+summary(m.group1)
+vif(m.group1) #no issues of collinearity here, remove confounding variable
+plot(m.group1)
+
+m.group2<-lm(CognitionZscore~ Gender+ClassYear+EarlyClass+WeekdaySleep+DASScore+Drinks+GPA,data=dat)
+summary(m.group2)
+vif(m.group2)
+plot(m.group2)
+
+#testing for relationship between classes missed and early classes
+m.c<-lm(ClassesMissed~ NumEarlyClass+EarlyClass, data=dat)
+summary(m.c) #significant intercept
+m.e<-lm(NumEarlyClass~ClassesMissed+EarlyClass, data=dat)
+summary(m.e)
+m.f<-lm(EarlyClass~NumEarlyClass+ClassesMissed, data=dat)
+summary(m.f) #intercept and numearlyclass significant
+
+#testing relation between sleeping
+m.g<-lm(PoorSleepQuality~WeekdayBed+WeekdayRise+WeekdaySleep+WeekendBed+WeekendRise+WeekendSleep+AverageSleep+AllNighter, data=dat)
+summary(m.g)
+m.h<-lm(PoorSleepQuality~WeekdayBed+WeekdayRise+WeekdaySleep+WeekendBed+WeekendRise+WeekendSleep+AverageSleep+AllNighter, data=dat)
+summary(m.h)
+m.i<-lm(PoorSleepQuality~AllNighter+AverageSleep,data=dat)
+summary(m.i)#intercept and average sleep
+m.j<-lm(PoorSleepQuality~WeekdaySleep+WeekendSleep+AllNighter, data=dat)
+summary(m.j) #weekday sleep and intercept highly significant
+m.k<-lm(WeekdaySleep~PoorSleepQuality+WeekendSleep+AllNighter,data=dat)
+summary(m.k)
 
 #testing for outliers in DASScore
 Das1<-lm(CognitionZscore~ DASScore, data=dat) 
@@ -47,3 +83,4 @@ summary(m.d)
 plot(m.d)
 #Test goodness of fit for LOGISTIC using: significance of coefficients, -2 log lieklihood/ deviance, classification table
 #test for linear: R sq adj R sq, F stat, adj SE, coeff fit, residual plot
+
